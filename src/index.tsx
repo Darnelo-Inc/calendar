fb65a10c61f7ded3f5c1e8524922cb60a6a81d51
Pavel Gorbunov <darnelo@inbox.ru>
2023-06-06 16:57:11 +0300

    gitignore changed

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import ReactDOM from "react-dom/client"
 import App from "./App"
 import { Provider } from "react-redux"
 import { store } from "./store"
-import { BrowserRouter } from "react-router-dom"
 import { HashRouter } from "react-router-dom"
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
